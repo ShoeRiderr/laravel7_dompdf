202fe374ef27b76104f45e5c48f759fbe6ee08b4
ShoeRiderr <kamilsocha20@gmail.com>
2020-07-29 10:19:28 +0200

    patient registration pannel html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,17 +6,152 @@
 <div class="container">
     <div class="card">
         <div class="card-body">
-            <div class="text-center mb-3">
-                <a href="/pdf-create"><button type="button" class="btn btn-success">Download PDF</button></a>
+            <form>
+                {{--  !!!!!!!!!!!!!!PATIENTS DATA!!!!!!!!!!!!!!!!!  --}}
+                <div class="form-row">
+                    <div class="form-group col-md-12">
+                        <h4>Patient data</h4>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label for="forname">Name</label>
+                      <input type="text" class="form-control" id="forname" name="forname" placeholder="Name">
+                  </div>
+                  <div class="form-group col-md-6">
+                      <label for="Surname">Surname</label>
+                      <input type="text" class="form-control" id="Surname" name="surname" placeholder="Surname">
+                  </div>
+                  <div class="form-group col-md-12">
+                    <label for="Pesel">Pesel</label>
+                    <input type="text" class="form-control" id="Pesel" name="pesel" placeholder="Pesel">
+                </div>
             </div>
-            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            {{-- !!!!!!!!!RESIDENCE ADDRESS!!!!!!!!!!!!!  --}}
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <h4>Place of residence</h4>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="Street">Street</label>
+                    <input type="text" class="form-control" id="Street" name="street" placeholder="Street">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="BuildNum">Building number</label>
+                    <input type="text" class="form-control" id="BuildNum" name="buildNum" placeholder="Building number">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="ApartNum">Apartment number</label>
+                    <input type="text" class="form-control" id="ApartNum" name="apartdNum" placeholder="Apartment number">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="City">City</label>
+                    <input type="text" class="form-control" id="City" name="city" placeholder="City">                           
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="InputProvince">Province</label>
+                    <select id="InputProvince" class="form-control">
+                        <option>dolnośląskie</option>
+                        <option>kujawsko-pomorskie</option>
+                        <option>lubelskie</option>
+                        <option>lubuskie</option>
+                        <option>łódzkie</option>
+                        <option>małopolskie</option>
+                        <option>mazowieckie</option>
+                        <option>opolskie</option>
+                        <option>podkarpackie</option>
+                        <option>podlaskie</option>
+                        <option>pomorskie</option>
+                        <option>śląskie</option>
+                        <option>świętokrzyskie</option>
+                        <option>warmińsko-mazurskie</option>
+                        <option>wielkopolskie</option>
+                        <option>zachodniopomorskie</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                 <label for="PostCode">Post code</label>
+                 <input type="text" class="form-control" id="PostCode" name="postCode" placeholder="Post Code">
+             </div>
+         </div>
+         {{--  !!!!!!!!!!!!!!CORRESPONDING ADDRESS!!!!!!!!!!!!!!!  --}}
+         <div class="form-row">
+            <div class="form-group col-md-12">
+                <h4>Corresponding address</h4>
+            </div>
+            {{--  !!!!!!!!!!!!!!!!!!!CHECKBOXES!!!!!!!!!!!!!!!  --}}
+            <div class="form-group col-md-12">
+                <div class="form-check form-check-inline">
+                    <input type="checkbox" class="form-check-input" id="CorrespondAsLiving" name="correspondAsLiving">
+                    <label for="CorrespondAsLiving">The same as residence address</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input type="checkbox" class="form-check-input" id="CorrespondDiff" name="correspondAsLiving">
+                    <label for="CorrespondAsLiving">Diffrent</label>
+                </div>
+            </div>
+            <div class="form-group col-md-8">
+                <label for="Street">Street</label>
+                <input type="text" class="form-control" id="Street" name="street" placeholder="Street">
+            </div>
+            <div class="form-group col-md-2">
+                <label for="BuildNum">Building number</label>
+                <input type="text" class="form-control" id="BuildNum" name="buildNum" placeholder="Building number">
+            </div>
+            <div class="form-group col-md-2">
+                <label for="ApartNum">Apartment number</label>
+                <input type="text" class="form-control" id="ApartNum" name="apartdNum" placeholder="Apartment number">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="City">City</label>
+                <input type="text" class="form-control" id="City" name="city" placeholder="City">                           
+            </div>
+            <div class="form-group col-md-4">
+                <label for="InputProvince">Province</label>
+                <select id="InputProvince" class="form-control">
+                    <option>dolnośląskie</option>
+                    <option>kujawsko-pomorskie</option>
+                    <option>lubelskie</option>
+                    <option>lubuskie</option>
+                    <option>łódzkie</option>
+                    <option>małopolskie</option>
+                    <option>mazowieckie</option>
+                    <option>opolskie</option>
+                    <option>podkarpackie</option>
+                    <option>podlaskie</option>
+                    <option>pomorskie</option>
+                    <option>śląskie</option>
+                    <option>świętokrzyskie</option>
+                    <option>warmińsko-mazurskie</option>
+                    <option>wielkopolskie</option>
+                    <option>zachodniopomorskie</option>
+                </select>
+            </div>
+            <div class="form-group col-md-2">
+             <label for="PostCode">Post code</label>
+             <input type="text" class="form-control" id="PostCode" name="postCode" placeholder="Post Code">
+         </div>
+     </div>
+     {{--  !!!!!!!!!!!!!!CONTACT!!!!!!!!!!!!!!  --}}
+     <div class="form-row">
+        <div class="form-group col-md-12">
+            <h4>Contact</h4>
         </div>
-    </div>
+        <div class="form-group col-md-8">
+            <label for="Email">Email</label>
+            <div class="input-group">
+                <input type="email" class="form-control" id="Email" name="email" placeholder="Email">
+                <div class="input-group-append">
+                 <span class="input-group-text">@example.com</span>
+             </div>
+         </div>
+     </div>
+     <div class="form-group col-md-4">
+         <label for="Phone">Phone</label>
+         <input type="text" class="form-control" id="Phone" name="phone" placeholder="Phone">
+     </div>
+ </div>
+</form>
+</div>
+</div>
 </div>
 
 @endsection
